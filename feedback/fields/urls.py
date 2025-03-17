9c89fe105692ba34ea5cba658870763b09fa90cf
Porozov Ivan <porozovivan090@gmail.com>
2025-03-17 20:56:10 +0200

    a new app added

--- feedback/fields/urls.py
+++ feedback/fields/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "fields"
 urlpatterns = [
     path('', views.read_comments, name='read_comments'),
     path('comments/', views.add_record, name='add_comment'),
